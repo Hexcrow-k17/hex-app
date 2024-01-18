c0b44f2cbfc96f6260e07ebe719a074b72edcfd7
Hexcrow-k17 <kimaleksander1702@gmail.com>
2024-01-18 12:58:54 +0100

    smaket på de

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import '../App.css';
+import TekstBoks from './TekstBols';
+
+function App() {
+  return (
+    <div className="h-screen bg-slate-800">
+      <h1>Hex App</h1>
+      <p>Dette er ...</p>
+      <TekstBoks />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/TekstBols.jsx
@@ -0,0 +1,9 @@
+function TekstBoks() {
+    return (
+        <div>
+            <h1>Hei, verden!</h1>
+        </div>
+    );
+}
+
+export default TekstBoks;
