7587afd768779fc86ff61576c72f8092699c3b04
Hexcrow-k17 <kimaleksander1702@gmail.com>
2024-01-25 11:17:44 +0100

    h

--- src/components/AnsattKort.jsx
+++ src/components/AnsattKort.jsx
@@ -1,13 +1,20 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPhone } from '@fortawesome/free-solid-svg-icons'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
-import { faUser } from '@fortawesome/free-solid-svg-icons'
-export default function AnsattKort(ansattinfo){
-    return(
-        <div className="ansattkort m-10 border-2 border-black p-5" >
-            <strong><FontAwesomeIcon icon={faUser} /> {ansattinfo.navn} </strong>
-            <p> <FontAwesomeIcon icon={ faPhone } /> {ansattinfo.tlf} </p>
-            <p><FontAwesomeIcon icon={faEnvelope} /> {ansattinfo.epost} </p>
-        </div>
-    )
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+export default function AnsattKort(ansattinfo) {
+  return (
+    <div className="ansattkort m-10 border-2 border-black p-5">
+      <strong>
+        <FontAwesomeIcon icon={faUser} /> {ansattinfo.navn}{" "}
+      </strong>
+      <p>
+        {" "}
+        <FontAwesomeIcon icon={faPhone} /> {ansattinfo.tlf}{" "}
+      </p>
+      <p>
+        <FontAwesomeIcon icon={faEnvelope} /> {ansattinfo.epost}{" "}
+      </p>
+    </div>
+  );
 }
--- src/components/TekstBols.jsx
+++ src/components/TekstBols.jsx
@@ -1,9 +1,9 @@
 function TekstBoks() {
-    return (
-        <div>
-            <h1>Hei, verden!</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Hei, verden!</h1>
+    </div>
+  );
 }
 
 export default TekstBoks;
