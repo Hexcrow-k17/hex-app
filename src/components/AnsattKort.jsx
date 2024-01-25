bbee0ebdcb282dd01dd9c683fb1788d93052b69f
Hexcrow-k17 <kimaleksander1702@gmail.com>
2024-01-25 10:43:20 +0100

    9

--- src/components/AnsattKort.jsx
+++ src/components/AnsattKort.jsx
@@ -1,9 +1,13 @@
-export default function AnsattKort(){
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPhone } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+export default function AnsattKort(ansattinfo){
     return(
-        <div className="ansatt-kort">
-            <strong>Navn:</strong>
-            <p>Telefon: </p>
-            <p>Epost: </p>
+        <div className="ansattkort m-10 border-2 border-black p-5" >
+            <strong><FontAwesomeIcon icon={faUser} /> {ansattinfo.navn} </strong>
+            <p> <FontAwesomeIcon icon={ faPhone } /> {ansattinfo.tlf} </p>
+            <p><FontAwesomeIcon icon={faEnvelope} /> {ansattinfo.epost} </p>
         </div>
     )
 }
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,53 @@
 import '../App.css';
 import AnsattKort from './AnsattKort';
-import TekstBoks from './TekstBols';
 
 function App() {
+
+  let ansatte = [
+    {
+      navn: "Kim Aleksander Persen",
+      tlf: "12 34 56 78",
+      epost: "kimppppp@gmail.com"
+    },
+    {
+      navn: "Jon Pape Hallem",
+      tlf: "62 94 73 94",
+      epost: "jonpape@gmail.com"
+    },
+    {
+      navn: "Roar Gjerde",
+      tlf: "93 85 72 04",
+      epost: "gjerde.roare@gmail.com"
+    },
+    {
+      navn: "Bent Erik Hammernes",
+      tlf: "93 75 26 17",
+      epost: "bent.erik@gmail.com"
+    },
+
+    {
+      navn: "Knut Eivind Brennhaug",
+      tlf: "62 81 01 82",
+      epost: "knuk.eb@gmail.com"
+    }
+
+  ];
+
   return (
-    <div className="h-screen bg-slate-800">
-      <h1>Hex App</h1>
-      <p>Dette er ...</p>
-      <AnsattKort />
+    <>
+    <h1 className=' text-center text-3xl text-red-600'>Hex-App</h1>
+    <div className=" grid grid-cols-3">
+
+      {ansatte.map(ansatte =>(
+        <AnsattKort navn={ansatte.navn} tlf={ansatte.tlf} epost={ansatte.epost} />
+      ))}
+
+      {/* <AnsattKort navn="Kim Aleksander Persen" tlf="12 34 56 78" epost="kimppppp@gmail.com" />
+      <AnsattKort navn="Jon Pape Hallem" tlf="62 94 73 94" epost="jonpape@gmail.com" />
+      <AnsattKort navn="Roar Gjerde" tlf="93 85 72 04" epost="gjerde.roar@gmail.com" />
+      <AnsattKort navn="Bent Erik Hammernes" tlf="93 75 26 17" epost="bent.erik@gmail.com" /> */}
     </div>
+    </>
   )
 }
 
